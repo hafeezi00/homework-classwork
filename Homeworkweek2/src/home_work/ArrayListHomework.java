e12cf68df835ee129b7e1a9fb6531456f252bc47
hafeezi00 <71222057+hafeezi00@users.noreply.github.com>
2020-10-03 18:49:43 -0400

    homework

--- Homeworkweek2/src/home_work/ArrayListHomework.java
+++ Homeworkweek2/src/home_work/ArrayListHomework.java
@@ -1,21 +1,24 @@
 package home_work;
 
 import java.util.ArrayList;
-import java.util.ArrayList;
 
 
 public class ArrayListHomework {
 
+	
+
 	public static void main(String[] args) {
 		
-	ArrayList<Integer> numbers= new ArrayList<Integer>();
+	ArrayList<Integer> nums= new ArrayList<Integer>();
 	
-	numbers.add(num1, 98);
+	 nums1 = (98,0,5);
 	
+     
+    
+    System.out.println(numbers1.get(0));
 
-	
-	}
-	System.out.println("ArrayList max value" + numbers.get );
-
-	}
+  
+    for(int i=0;i<numbers1.size();i++)
+		System.out.println("ArrayList max value = " + numbers1[numbers1,length];
+}
 }
